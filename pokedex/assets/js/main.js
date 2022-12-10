76dd7bfbed6a2804dd08600740baeb8c74d944bd
alamhtrindade <alamhtrindade@gmail.com>
2022-12-09 23:30:50 -0400

    insert automatic info pokemons

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,25 @@
-console.log("Sucesso");
+const pokemonList = document.getElementById("pokemonList")
+
+function convertPokemonToList(pokemon){
+    return `
+        <li class="pokemon ${pokemon.type}">
+                    
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+
+        </li>
+    `
+}
+
+pokeApi.getPokemons().then((pokemons = [])   =>   {
+    
+    pokemonList.innerHTML += pokemons.map(convertPokemonToList).join(``)
+
+})
---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,37 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    
+    const pokemon = new Pokemon()
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type1] = types
+
+    pokemon.number = pokeDetail.order
+    pokemon.name   = pokeDetail.name
+    pokemon.types  = types
+    pokemon.type   = type1
+    pokemon.photo  = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon => {
+    
+    return fetch(pokemon.url)
+            .then((response)=> response.json())
+            .then(convertPokeApiDetailToPokemon)
+
+})
+
+pokeApi.getPokemons = (offset = 0,limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    
+    return fetch(url)
+        .then((response)        =>   response.json())
+        .then((jsonBody)        =>   jsonBody.results)
+        .then((pokemons)        =>   pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest)   =>   Promise.all(detailRequest))
+        .then((pokemonsDetails) =>   pokemonsDetails)
+        .catch((error)          =>   console.error(error))
+
+}
